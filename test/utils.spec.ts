2524f1804c558b64440a313dc88044413f9a566f
m03geek <928021-m03geek@users.noreply.gitlab.com>
2019-06-02 00:21:27 +0300

    test(utils): add tests

---
+++ test/utils.spec.ts
@@ -0,0 +1,79 @@
+import 'jest';
+import {mergeRoles, flatten} from '../src/utils';
+
+describe('utils', () => {
+  describe('mergeRoles', () => {
+    const foo = {
+      a: 1,
+      b: {
+        c: 1,
+        d: 2,
+      },
+    };
+
+    const bar = {
+      a: 2,
+      b: {
+        d: 3,
+        e: 4,
+      },
+    };
+
+    const foobar = {
+      a: 2,
+      b: {
+        c: 1,
+        d: 3,
+        e: 4,
+      },
+    };
+    test('merges object', async () => {
+      expect(mergeRoles({}, foo, bar)).toEqual(foobar);
+    });
+  });
+  describe('flatten', () => {
+    const foo = {
+      a: {
+        b: {
+          c: 1,
+          d: {
+            e: 2,
+          },
+        },
+        f: 3,
+      },
+      g: 4,
+    };
+
+    const fooFlat = {
+      'a.b.c': 1,
+      'a.b.d.e': 2,
+      'a.f': 3,
+      g: 4,
+    };
+
+    const fooFlatSep = {
+      'a:b:c': 1,
+      'a:b:d:e': 2,
+      'a:f': 3,
+      g: 4,
+    };
+
+    const invalid = {
+      a: undefined,
+      b: null,
+    };
+
+    test('default separator', async () => {
+      expect(flatten(foo)).toEqual(fooFlat);
+    });
+
+    test('custom separator', async () => {
+      expect(flatten(foo, ':')).toEqual(fooFlatSep);
+    });
+
+    test('invalid object', async () => {
+      expect(flatten(invalid)).toEqual(invalid);
+    });
+  });
+});
