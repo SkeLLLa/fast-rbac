3b603ef7ea0f080f8fae39103633c8e06f6e9d97
m03geek <928021-m03geek@users.noreply.gitlab.com>
2019-06-03 11:06:43 +0300

    test(update): add add/remove tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,109 +1,110 @@
+// tslint:disable: no-string-literal
+
 import 'jest';
 import RBAC from '../src/index';
 
 describe('rbac', () => {
-  const basic = new RBAC({
-    roles: {
-      guest: {
-        can: ['foo:read'],
-      },
-      user: {
-        can: [
-          {
-            name: 'foo:update',
-          },
-        ],
-      },
-      admin: {
-        can: [
-          {
-            name: 'foo',
-            operation: 'remove',
-          },
-        ],
-      },
-      superadmin: {
-        can: [
-          {
-            name: 'foo',
-            operation: 'create',
-            when: (ctx) => {
-              return ctx.userId === ctx.ownerId;
-            },
-          },
-        ],
-      },
-    },
-  });
-
-  const wildcard = new RBAC({
-    roles: {
-      guest: {
-        can: ['*:read'],
-      },
-      user: {
-        can: ['foo:*'],
-      },
-      admin: {
-        can: ['*'],
-      },
-    },
-  });
-
-  const wildcardNoMemo = new RBAC({
-    roles: {
-      user: {
-        can: ['foo:*'],
-      },
-    },
-    memoize: false,
-  });
-
-  const hierarchical = new RBAC({
-    roles: {
-      guest: {
-        can: ['foo:read'],
-      },
-      anon: {
-        can: [],
-        inherits: ['guest'],
-      },
-      user: {
-        can: [
-          {
-            name: 'foo:update',
-          },
-        ],
-        inherits: ['guest'],
-      },
-      admin: {
-        can: [
-          {
-            name: 'foo',
-            operation: 'remove',
-          },
-        ],
-        inherits: ['user'],
-      },
-      superadmin: {
-        can: [
-          {
-            name: 'foo',
-            operation: 'create',
-            when: (ctx) => {
-              return ctx.userId === ctx.ownerId;
-            },
-          },
-        ],
-        inherits: ['admin'],
-      },
-    },
-  });
-
   describe('can', () => {
-    // tslint:disable: no-string-literal
+    const basic = new RBAC({
+      roles: {
+        guest: {
+          can: ['foo:read'],
+        },
+        user: {
+          can: [
+            {
+              name: 'foo:update',
+            },
+          ],
+        },
+        admin: {
+          can: [
+            {
+              name: 'foo',
+              operation: 'remove',
+            },
+          ],
+        },
+        superadmin: {
+          can: [
+            {
+              name: 'foo',
+              operation: 'create',
+              when: (ctx) => {
+                return ctx.userId === ctx.ownerId;
+              },
+            },
+          ],
+        },
+      },
+    });
+
+    const wildcard = new RBAC({
+      roles: {
+        guest: {
+          can: ['*:read'],
+        },
+        user: {
+          can: ['foo:*'],
+        },
+        admin: {
+          can: ['*'],
+        },
+      },
+    });
+
+    const wildcardNoMemo = new RBAC({
+      roles: {
+        user: {
+          can: ['foo:*'],
+        },
+      },
+      memoize: false,
+    });
+
+    const hierarchical = new RBAC({
+      roles: {
+        guest: {
+          can: ['foo:read'],
+        },
+        anon: {
+          can: [],
+          inherits: ['guest'],
+        },
+        user: {
+          can: [
+            {
+              name: 'foo:update',
+            },
+          ],
+          inherits: ['guest'],
+        },
+        admin: {
+          can: [
+            {
+              name: 'foo',
+              operation: 'remove',
+            },
+          ],
+          inherits: ['user'],
+        },
+        superadmin: {
+          can: [
+            {
+              name: 'foo',
+              operation: 'create',
+              when: (ctx) => {
+                return ctx.userId === ctx.ownerId;
+              },
+            },
+          ],
+          inherits: ['admin'],
+        },
+      },
+    });
+
     describe('basic', () => {
-      test('can:nonexist:false', () => {
+      test('nonexist:can:false', () => {
         expect(basic.can('somebody', 'foo', 'read')).toEqual(false);
         expect(basic['_rulesCompiled']['somebody:foo:read']).toBeUndefined();
       });
@@ -288,6 +289,146 @@ describe('rbac', () => {
     });
   });
 
-  describe('add', () => {});
-  describe('remove', () => {});
+  describe('add', () => {
+    test('basic:can:true', () => {
+      const rbac = new RBAC();
+      rbac.add('guest', 'foo', 'read');
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(true);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toEqual(true);
+    });
+    test('wildcard:can:true', () => {
+      const rbac = new RBAC();
+      rbac.add('guest', 'foo', '*');
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(true);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toEqual(true);
+    });
+    test('wildcard:can:true', () => {
+      const rbac = new RBAC();
+      rbac.add('guest', '*', 'read');
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(true);
+      expect(rbac.can('guest', 'bar', 'read')).toEqual(true);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toEqual(true);
+      expect(rbac['_rulesCompiled']['guest:bar:read']).toEqual(true);
+    });
+    test('hierarchical:can:true', () => {
+      const rbac = new RBAC({
+        roles: {
+          guest: {
+            can: ['foo:read'],
+          },
+          user: {
+            can: ['foo:update'],
+            inherits: ['guest'],
+          },
+          admin: {
+            can: ['foo:remove'],
+            inherits: ['user'],
+          },
+        },
+      });
+      rbac.add('guest', 'foo', 'remove');
+      rbac.add('user', 'foo', 'create', (ctx) => {
+        return ctx.a === ctx.b;
+      });
+
+      expect(rbac.can('guest', 'foo', 'remove')).toEqual(true);
+      expect(rbac['_rulesCompiled']['guest:foo:remove']).toEqual(true);
+      expect(rbac.can('user', 'foo', 'remove')).toEqual(true);
+      expect(rbac['_rulesCompiled']['user:foo:remove']).toEqual(true);
+      expect(rbac.can('admin', 'foo', 'create')).toEqual(true);
+      expect(rbac['_rulesCompiled']['admin:foo:create']).toBeDefined();
+      expect(rbac.can('admin', 'foo', 'create', {a: 1, b: 1})).toEqual(true);
+      expect(rbac.can('admin', 'foo', 'create', {a: 1, b: 2})).toEqual(false);
+    });
+  });
+  describe('remove', () => {
+    test('basic:can:false', () => {
+      const rbac = new RBAC({
+        roles: {
+          guest: {
+            can: ['foo:read'],
+          },
+        },
+      });
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(true);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toEqual(true);
+      rbac.remove('guest', 'foo', 'read');
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(false);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toBeUndefined();
+    });
+    test('wildcard:can:false', () => {
+      const rbac = new RBAC({
+        roles: {
+          guest: {
+            can: ['foo:read'],
+          },
+        },
+      });
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(true);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toEqual(true);
+      rbac.remove('guest', 'foo');
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(false);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toBeUndefined();
+    });
+    test('wldcard:can:false', () => {
+      const rbac = new RBAC({
+        roles: {
+          guest: {
+            can: ['foo:read'],
+          },
+        },
+      });
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(true);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toEqual(true);
+      rbac.remove('guest');
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(false);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toBeUndefined();
+    });
+    test('wldcard:can:false', () => {
+      const rbac = new RBAC({
+        roles: {
+          guest: {
+            can: ['foo:read'],
+          },
+        },
+      });
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(true);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toEqual(true);
+      rbac.remove('guest', '*', 'read');
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(false);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toBeUndefined();
+    });
+    test('wldcard:can:false', () => {
+      const rbac = new RBAC({
+        roles: {
+          guest: {
+            can: ['*:read'],
+          },
+        },
+      });
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(true);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toEqual(true);
+      rbac.remove('guest', '*', 'read');
+      expect(rbac.can('guest', 'foo', 'read')).toEqual(false);
+      expect(rbac['_rulesCompiled']['guest:foo:read']).toBeUndefined();
+    });
+    test('hierarchical:can:false', () => {
+      const rbac = new RBAC({
+        roles: {
+          guest: {
+            can: ['foo:read'],
+          },
+          user: {
+            can: ['foo:update'],
+            inherits: ['guest']
+          }
+        },
+      });
+      expect(rbac.can('user', 'foo', 'read')).toEqual(true);
+      expect(rbac['_rulesCompiled']['user:foo:read']).toEqual(true);
+      rbac.remove('guest', 'foo', 'read');
+      expect(rbac.can('user', 'foo', 'read')).toEqual(false);
+      expect(rbac['_rulesCompiled']['user:foo:read']).toBeUndefined();
+    });
+  });
 });
